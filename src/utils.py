5b7fa27d5f496d1a7f67c18034835a4b3f2bdf4f
Pavlo Kuptsov <pavlo.kupstov@sigma.software>
2024-12-16 02:29:14 +0200

    Fix for month number

--- src/utils.py
+++ src/utils.py
@@ -11,8 +11,8 @@ def get_latest_db_file():
 
 def timeframe_to_dates(year: int, month: Optional[int] = None) -> tuple[datetime, datetime]:
     if month:
-        d_from = datetime(year, month % 12, 1)
-        d_to = datetime(year, (month + 1) % 12, 1)
+        d_from = datetime(year, month % 12 if month > 12 else month , 1)
+        d_to = datetime(year, (month + 1) % 12 if month > 11 else month, 1)
     else:
         d_from = datetime(year, 1, 1)
         d_to = datetime(year + 1, 1, 1)
