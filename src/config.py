60914f50508d9cda55a88063dcb30ad5bcb52e4d
Pavlo Kuptsov <pavlo.kupstov@sigma.software>
2025-07-16 01:45:30 +0300

    Fix for missing exchange rate at night

--- src/config.py
+++ src/config.py
@@ -1,9 +1,9 @@
-from datetime import datetime
-
+from datetime import datetime, timedelta
 
 BURN_RATE_AMOUNT_THRESHOLD = 4000
 MAX_TRANSACTION_AMOUNT_THRESHOLD = 1_000_000
 DATA_PATH = '/app/data'
 PREPARATORY_QUERIES_FILE = 'sql/preparatory_queries.sql'
+EXCHANGE_RATE_DATE = datetime.now() if datetime.now().hour > 8 else datetime.now() - timedelta(days=1)
 PB_EXCHANGE_ENDPOINT = 'https://api.privatbank.ua/p24api/exchange_rates?json&date={}'
-EXC_RATE_ENDPOINT = PB_EXCHANGE_ENDPOINT.format(datetime.now().strftime('%d.%m.%Y'))
+EXC_RATE_ENDPOINT = PB_EXCHANGE_ENDPOINT.format(EXCHANGE_RATE_DATE.strftime('%d.%m.%Y'))
